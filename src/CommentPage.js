2cd869a91f853b1410edc78c97f5873c4dbda2e9
rkun123 <tren111213@gmail.com>
2020-12-22 02:10:06 +0900

    [add] slide animation

--- src/CommentPage.js
+++ src/CommentPage.js
@@ -19,7 +19,11 @@ class CommentPage extends Component {
     this.state = {
       account: 'アルティメッ鳩',
       content: 'アニメーション微塵も思いつかなくて草',
+      comments: [],
+      isStreaming: false,
+      streaming: null
     };
+    this.moveComment.play()
     this.rewrite = this.rewrite.bind(this);
     this.getName = this.getName.bind(this);
     this.streamStart = this.streamStart.bind(this);
@@ -32,7 +36,7 @@ class CommentPage extends Component {
     txt = txt.replace('<br />','　')
     txt = txt.replace('&apos;', '\'')
     txt = txt.replace('&amp;', '&')
-    txt = txt.replace('&quot;', '\"')
+    txt = txt.replace('&quot;', '"')
     return txt
   }
 
@@ -45,14 +49,23 @@ class CommentPage extends Component {
   }
 
   streamStart(){
+    console.log('Start streaming')
     this.mstdn.stream('streaming/public/local').on('message', (msg) => {
       if (msg.event === "update"){
+        console.log(msg.data.content)
         this.setState({
           account: this.getName(msg.data.account),
           content: this.rewrite(msg.data.content),
         })
-        this.moveComment.play();
-        console.log(this.moveComment);
+        let { comments } = this.state
+        const comment = (
+          <div className="comment">
+            <p id="content">{ this.getName(msg.data.content)}</p>
+            <p id="user">{ this.rewrite(msg.data.account )}</p>
+          </div>
+        )
+        comments.push(comment)
+        this.setState({comments})
       }
     })
   }
@@ -60,11 +73,12 @@ class CommentPage extends Component {
   render() {
     return (
       <div>
-        <div class='screen'>
-          <div class="comment">
-            <p id="content">{this.state.content}</p>
-            <p id="user">{this.state.account}</p>
-          </div>
+        <div className='screen'>
+          {
+            this.state.comments.map((comment) => {
+              return comment
+            })
+          }
         </div>
         <input type='button' value='LTLの監視開始' onClick={this.streamStart()} />
       </div>
